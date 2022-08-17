f31ffabfe657c02170a2bf4ebcdc7dd739e7a934
SA17 <108982682+SA-17@users.noreply.github.com>
2022-08-17 23:22:10 +0100

    Update and rename omni_a10s.mk to omni_m10lte.mk

--- omni_m10lte.mk
+++ omni_m10lte.mk
@@ -21,17 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from a10s device
-$(call inherit-product, device/samsung/a10s/device.mk)
+$(call inherit-product, device/samsung/m10lte/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a10s
-PRODUCT_NAME := omni_a10s
+PRODUCT_DEVICE := m10lte
+PRODUCT_NAME := omni_m10lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := Galaxy A10s
+PRODUCT_MODEL := Galaxy M10
 
 # Resolution
 TARGET_SCREEN_WIDTH := 720
