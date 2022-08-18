72d66d430fb22720ebf99d8670b41e8780899376
SA17 <108982682+SA-17@users.noreply.github.com>
2022-08-18 22:20:26 +0100

    Update and rename omni_m10lte.mk to twrp_m10lte.mk

--- twrp_m10lte.mk
+++ twrp_m10lte.mk
@@ -24,8 +24,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/m10lte/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/zImage:kernel \
