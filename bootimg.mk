24b1e28ab904954ae773bdf0696f2a0ab0700aa2
SA17 <108982682+SA-17@users.noreply.github.com>
2022-08-18 13:35:38 +0100

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_kernel) $(recovery_ramdisk)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) device/samsung/m10lte/dt.img $(recovery_kernel) $(recovery_ramdisk)
 	@echo -e ${CL_GRN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(recovery_ramdisk)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
